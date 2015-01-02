00c947d60aa92f1e7d89f5d63ee57d28b67a15de
Michael Alger <mike.t.alger@gmail.com>
2015-01-02 03:43:00 -0500

    Added a class that will allow me to diff 2 dict and get the create, remove, and update values.

--- mysite/getdbinfo/management/commands/importdata.py
+++ mysite/getdbinfo/management/commands/importdata.py
@@ -9,22 +9,52 @@ from getdbinfo.models import People
 import xml.etree.ElementTree as et
 import json
 
+class Difflist(object):
+    self.create = []
+    self.remove = []
+    self.update = []
+
+    def __init__(self, new, old):
+        #first we need to remove the records that will not be changing.
+        for new_item in new:
+            if new_item in old:
+                new.remove(old.pop(old.index(new_item)))
+        
+        #list to update
+        for old_item in old:
+            found = False
+            for new_item in new:
+                if new_item['id'] == old_item['id']:
+                    found = True
+                    self.update.append(new_item)
+                    new.remove(new_item)
+                    old.remove(old_item)
+    
+        self.remove = old
+        self.create = new
+
 class Command(BaseCommand):
     args = 'none required'
     help = 'Imports the people data'
     
     def handle(self, *args, **options):
-        data_list = et.fromstring('<data><person><first_name>Jim</first_name><home/><last_name>Johns</last_name><age>66</age></person><person><first_name>Larry</first_name><home/><last_name>Smith</last_name><age>34</age></person></data>')
+        new_people_list = []
+        data_tree = et.fromstring('<data><person><first_name>Jim</first_name><home/><last_name>Johns</last_name><age>44</age></person><person><first_name>Larry</first_name><home/><last_name>Smith</last_name><age>34</age></person></data>')
+        full_list_of_people = People.objects.all()
 
-        for person in data_list.findall('person'):
+
+        for person in data_tree.findall('person'):
             items = {}
             for field in person.iter():
                 if not (field.text is None or field.tag == "person"):
                     items[field.tag] = field.text
+            new_people_list.append(items)
             
             self.stdout.write(json.dumps(items))
             try:
-                people = People(**items)
+                people = People.objects.update_or_create(**items)
                 people.save()
             except IntegrityError as e:
-                raise CommandError('("{}", "{}"): {} '.format(people.first_name, people.last_name, e))
+                #raise CommandError('{} '.format(e))
+                self.stdout.write('{}'.format(e))
+                pass
